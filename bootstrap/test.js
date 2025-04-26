263ae55658421cdd68646a00d0e843e269a47de9
htetwaiyanjs <myaingyztoatgyi18@gmail.com>
2025-04-26 15:02:08 +0630

    jf

---
+++ bootstrap/test.js
@@ -0,0 +1,15 @@
+
+function hey (){
+    let text = document.getElementById('text').value;
+
+    let p = document.getElementsByClassName('p').value
+    console.log(text);
+     let j =20
+    for(let i=0;i<j;i++) {
+      p.innerHTML = text
+      
+        
+    }
+}
+
+// var name = document.getElementById('name').value;
